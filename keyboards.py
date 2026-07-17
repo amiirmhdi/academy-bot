67bde8be68d7dcffe604682c1c2b6ab84f724954
amiirmhdi <amirmahdihasanzade1381@gmail.com>
2026-07-17 15:54:36 +0330

    Add files via upload

--- keyboards.py
+++ keyboards.py
@@ -1,29 +1,15 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
 def main_menu():
     markup = InlineKeyboardMarkup(row_width=2)
 
     markup.row(
         InlineKeyboardButton("📚 دوره‌ها", callback_data="courses"),
-        InlineKeyboardButton("⭐️ نظر شما", callback_data="feedback")
+        InlineKeyboardButton("⭐ نظر شما", callback_data="feedback")
     )
 
     markup.row(
         InlineKeyboardButton("👩🏻‍🏫 مشاوره", callback_data="advisor")
-    )
-
-    return markup
-
-
-def admin_close_btn(user_id):
-    markup = InlineKeyboardMarkup()
-
-    markup.add(
-        InlineKeyboardButton(
-            "🔒 بستن تیکت",
-            callback_data=f"admin_close_{user_id}"
-        )
     )
 
     return markup
