fa37704a0ffcc8ca4ce2fa9ca830f0c2bf8a361b
amiirmhdi <amirmahdihasanzade1381@gmail.com>
2026-07-19 00:28:09 +0330

    Add files via upload

--- database.py
+++ database.py
@@ -1,54 +1,79 @@
-import sqlite3
-import os
+import psycopg2
+from config import DATABASE_URL
 
-DB_NAME = "arakbot.db"
 
-print("DB PATH:", os.path.abspath(DB_NAME))
+def get_connection():
+
+    return psycopg2.connect(DATABASE_URL)
+
 
 def init_db():
 
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_connection()
     cur = conn.cursor()
 
     cur.execute("""
     CREATE TABLE IF NOT EXISTS users(
-        chat_id INTEGER PRIMARY KEY,
+        chat_id BIGINT PRIMARY KEY,
         first_name TEXT,
         username TEXT
-    )
+    );
     """)
 
-    cur.execute("SELECT COUNT(*) FROM users")
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS tickets(
+        id SERIAL PRIMARY KEY,
+        user_id BIGINT,
+        status TEXT
+    );
+    """)
 
-    print("TOTAL USERS IN DB:", cur.fetchone()[0])
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS messages(
+        id SERIAL PRIMARY KEY,
+        ticket_id INTEGER,
+        sender TEXT,
+        text TEXT
+    );
+    """)
 
     conn.commit()
+
+    cur.close()
     conn.close()
 
 
 def add_user(chat_id, first_name, username):
 
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_connection()
     cur = conn.cursor()
 
     cur.execute("""
-    INSERT OR IGNORE INTO users(chat_id,first_name,username)
-    VALUES(?,?,?)
-    """, (chat_id, first_name, username))
+    INSERT INTO users(chat_id, first_name, username)
+    VALUES (%s,%s,%s)
+    ON CONFLICT(chat_id)
+    DO NOTHING;
+    """,(chat_id,first_name,username))
 
     conn.commit()
+
+    cur.close()
     conn.close()
 
 
 def get_all_users():
 
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_connection()
     cur = conn.cursor()
 
-    cur.execute("SELECT chat_id FROM users")
+    cur.execute("""
+    SELECT chat_id
+    FROM users
+    """)
 
     users = cur.fetchall()
 
+    cur.close()
     conn.close()
 
     return users
@@ -56,16 +81,19 @@ def get_all_users():
 
 def get_open_ticket(user_id):
 
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_connection()
     cur = conn.cursor()
 
     cur.execute("""
-    SELECT id FROM tickets
-    WHERE user_id=? AND status='open'
+    SELECT id
+    FROM tickets
+    WHERE user_id=%s
+    AND status='open'
     """, (user_id,))
 
     ticket = cur.fetchone()
 
+    cur.close()
     conn.close()
 
     return ticket
@@ -73,18 +101,20 @@ def get_open_ticket(user_id):
 
 def create_ticket(user_id):
 
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_connection()
     cur = conn.cursor()
 
     cur.execute("""
     INSERT INTO tickets(user_id,status)
-    VALUES(?,?)
+    VALUES(%s,%s)
+    RETURNING id
     """, (user_id, "open"))
+
+    ticket_id = cur.fetchone()[0]
 
     conn.commit()
 
-    ticket_id = cur.lastrowid
-
+    cur.close()
     conn.close()
 
     return ticket_id
@@ -92,28 +122,33 @@ def create_ticket(user_id):
 
 def close_ticket(user_id):
 
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_connection()
     cur = conn.cursor()
 
     cur.execute("""
     UPDATE tickets
     SET status='closed'
-    WHERE user_id=? AND status='open'
+    WHERE user_id=%s
+    AND status='open'
     """, (user_id,))
 
     conn.commit()
+
+    cur.close()
     conn.close()
 
 
 def save_message(ticket_id, sender, text):
 
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_connection()
     cur = conn.cursor()
 
     cur.execute("""
     INSERT INTO messages(ticket_id, sender, text)
-    VALUES(?,?,?)
+    VALUES (%s, %s, %s)
     """, (ticket_id, sender, text))
 
     conn.commit()
-    conn.close()
+
+    cur.close()
+    conn.close()    
